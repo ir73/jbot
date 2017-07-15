591dfbbf1529eb5bc474d7bec591af129ca2f3e2
Sergei Ledvanov <sergei.ledvanov@transferwise.com>
2017-07-15 18:55:17 +0300

    - added missing properties

--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/models/RichMessage.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/models/RichMessage.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,14 +18,19 @@ import java.util.Map;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class RichMessage {
+public class RichMessage implements Serializable {
     private String token;
     private String username;
     @JsonProperty("icon_emoji")
     private String iconEmoji;
     private String channel;
     private String text;
+    private String ts;
     private Boolean mrkdwn;
+    @JsonProperty("link_names")
+    private Boolean linkNames;
+    @JsonProperty("as_user")
+    private Boolean asUser;
     @JsonProperty("response_type")
     private String responseType;
     private Attachment[] attachments;
@@ -63,11 +69,20 @@ public class RichMessage {
         if (responseType != null) {
             map.put("responseType", responseType);
         }
-        if (attachments != null && attachments.length > 0) {
+        if (attachments != null) {
             map.put("attachments", mapper.writeValueAsString(attachments));
         }
         if (mrkdwn != null) {
             map.put("mrkdwn", mrkdwn);
+        }
+        if (asUser != null) {
+            map.put("as_user", asUser);
+        }
+        if (ts != null) {
+            map.put("ts", ts);
+        }
+        if (linkNames != null) {
+            map.put("link_names", linkNames);
         }
         return map;
     }
